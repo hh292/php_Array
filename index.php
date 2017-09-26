ef0d357e5ed7b53b9e9919f931f08b05d8c93ac4
Himanshu Hunge <hh292@afsconnect1.njit.edu>
2017-09-25 22:09:14 -0400

    Sixth Array function

--- index.php
+++ index.php
@@ -22,11 +22,13 @@
   $in_array_3 = array(6, "A", 6, "A", "C", "D", "D");
   $in_array_4 = array(2, 4, 6, 8);
   $in_array_5 = array("Name" => "Himanshu", "Height" => "181 cm", "UCID" => "hh292");
+  $in_array_6 = array(0 => 'Tennis', 1 => 'Baseball', 2 => 'Cricket', 3 => 'Hockey');
   $obj->print_1($in_array_1);
   $obj->print_2($in_array_2);
   $obj->print_3($in_array_3);
   $obj->print_4($in_array_4);
   $obj->print_5($in_array_5);
+  $obj->print_6($in_array_6);
   
 
   class main {
@@ -116,11 +118,24 @@
         print_r($domain); 
     }
 
-   
+   // function 6 : strtoupper ...hh292
+  
+    public function print_6($in_array_6)
+    {
+        echo'<hr>';
+        echo'<h2> Function Name : array_search </h2></br>'; 
+        echo 'Description:Searches the array for a given value and returns the first corresponding key if successful. Here, we are searching for <b><i>Cricket</i></b> :' ."\r"; 
+        echo '</br>Input --->';
+        print_r($in_array_6); 
+        $domain = array_search('Cricket', $in_array_6 );
+        echo '</br>Output --->';
+        print_r($domain);
+    }
+    
      public function __destruct() {
 
      echo '</br> <h3>Finally I\'m Done with string functions. </h3> </br>';
-     echo '</br> ***********************Copyright Assignment @Himanshu Hunge (hh292)***************************';
+     echo '</br> *************************Copyright Assignment @Himanshu Hunge (hh292)***************************';
   
    } 
 }
